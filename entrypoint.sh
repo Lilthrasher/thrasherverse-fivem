e4528d817a4926d1d3c6df1f2b2bb7eee051c22d
Kyle Smith <kyle.smith.andrew@gmail.com>
2025-07-25 15:41:21 +0000

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/fivem/server-data
+
+if [ ! -f server.cfg ]; then
+    echo "No server.cfg found in server-data!"
+    exit 1
+fi
+
+/home/fivem/server/run.sh +exec server.cfg
