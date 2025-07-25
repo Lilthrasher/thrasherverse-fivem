6f86657363286cd12749b5ddfb036b4620597a13
Kyle Smith <kyle.smith.andrew@gmail.com>
2025-07-25 22:25:03 +0000

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-cd /home/fivem/server-data
-
-# if [ ! -f server.cfg ]; then
-#     echo "No server.cfg found in server-data!"
-#     exit 1
-# fi
-
-# /home/fivem/server/run.sh +exec server.cfg
-
 /home/fivem/server/run.sh
