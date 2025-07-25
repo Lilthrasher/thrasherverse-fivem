b8dbb053626caea66c6c4024595213b083afc384
Kyle Smith <kyle.smith.andrew@gmail.com>
2025-07-25 15:57:15 +0000

    Updated DOCKERFILE and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 cd /home/fivem/server-data
 
-if [ ! -f server.cfg ]; then
-    echo "No server.cfg found in server-data!"
-    exit 1
-fi
+# if [ ! -f server.cfg ]; then
+#     echo "No server.cfg found in server-data!"
+#     exit 1
+# fi
 
-/home/fivem/server/run.sh +exec server.cfg
+# /home/fivem/server/run.sh +exec server.cfg
+
+/home/fivem/server/run.sh
